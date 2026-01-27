4b44871cc0337779d12c219fee50d748065f3a84
Frederic BIDON <fredbi@yahoo.com>
2026-01-28 00:00:17 +0100

    chore: regen everything (without commit sha)
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- require/require_examples_test.go
+++ require/require_examples_test.go
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Code generated with github.com/go-openapi/testify/codegen/v2; DO NOT EDIT.
-// Generated on 2026-01-27 (version 98658ef) using codegen version v2.2.1-0.20260127181549-98658ef85ebb [sha: 98658ef85ebb5f0990ed1c8408af6defef6c6d5c]
 
 package require_test
 
