4a93a544794ee1aa63a6254146e98e6d64884aba
Frederic BIDON <fredbi@yahoo.com>
2026-02-06 00:48:16 +0100

    feat: NoGoRoutineLeak
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- internal/assertions/safety.go
+++ internal/assertions/safety.go
@@ -3,38 +3,61 @@
 
 package assertions
 
-import "github.com/go-openapi/testify/v2/internal/leak"
+import (
+	"context"
 
-type (
-	LeakOption leakOption
-	leakOption leak.Option
+	"github.com/go-openapi/testify/v2/internal/leak"
 )
 
 // NoGoRoutineLeak ensures that no goroutine did leak from inside the tested function.
 //
-// The function passed may apply optional filters to exclude known false positives (e.g. http, database connection...).
+// NOTE: only the go routines spawned from inside the tested function are checked for leaks.
+// No filter or configuration is needed to exclude "known go routines".
+//
+// Resource cleanup should be done inside the tested function, and not using [testing.T.Cleanup],
+// as t.Cleanup is called after the leak check.
+//
+// # Edge cases
+//
+//   - if the tested function panics leaving behind leaked goroutines, these are detected.
+//   - if the tested function calls runtime.Goexit (e.g. from [testing.T.FailNow]) leaving behind leaked goroutines,
+//     these are detected.
+//   - if a panic occurs in one of the leaked go routines, it cannot be recovered with certainty and
+//     the calling program will usually panic.
+//
+// # Concurrency
+//
+// [NoGoRoutineLeak] may be used safely in parallel tests.
 //
 // # Usage
 //
-//	NoGoRoutineLeak(t, func() {
-//	},
-//	"should not leak any goroutine",
-//	)
+//		NoGoRoutineLeak(t, func() {
+//			...
+//		},
+//	 nil,
+//		"should not leak any go routine",
+//		)
 //
 // # Examples
 //
 //   - success: NOT IMPLEMENTED
-func NoGoRoutineLeak(t T, inside func(options ...LeakOption), msgAndArgs ...any) bool {
+func NoGoRoutineLeak(t T, tested func(), msgAndArgs ...any) bool {
 	// Domain: safety
-	_ = inside // TODO
 	if h, ok := t.(H); ok {
 		h.Helper()
 	}
 
-	err := leak.Find()
-	if err == nil {
+	var ctx context.Context
+	if c, ok := t.(contextualizer); ok {
+		ctx = c.Context()
+	} else {
+		ctx = context.Background()
+	}
+
+	signature := leak.Leaked(ctx, tested)
+	if signature == "" {
 		return true
 	}
 
-	return Fail(t, err.Error(), msgAndArgs...)
+	return Fail(t, "found leaked go routines: "+signature, msgAndArgs...)
 }
