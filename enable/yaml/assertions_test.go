2b964455c20bf57bd66b7d7f8496daa0f9cd0bb1
Frederic BIDON <fredbi@yahoo.com>
2026-02-08 13:24:02 +0100

    test(enable): completed tests for enabled optional features
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- enable/yaml/assertions_test.go
+++ enable/yaml/assertions_test.go
@@ -12,78 +12,78 @@ import (
 func TestYAMLEq_EqualYAMLString(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT, `{"hello": "world", "foo": "bar"}`, `{"hello": "world", "foo": "bar"}`))
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock, `{"hello": "world", "foo": "bar"}`, `{"hello": "world", "foo": "bar"}`))
 }
 
 func TestYAMLEq_EquivalentButNotEqual(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT, `{"hello": "world", "foo": "bar"}`, `{"foo": "bar", "hello": "world"}`))
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock, `{"hello": "world", "foo": "bar"}`, `{"foo": "bar", "hello": "world"}`))
 }
 
 func TestYAMLEq_HashOfArraysAndHashes(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT, expectedYAML, actualYAML))
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock, expectedYAML, actualYAML))
 }
 
 func TestYAMLEq_Array(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT, `["foo", {"hello": "world", "nested": "hash"}]`, `["foo", {"nested": "hash", "hello": "world"}]`))
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock, `["foo", {"hello": "world", "nested": "hash"}]`, `["foo", {"nested": "hash", "hello": "world"}]`))
 }
 
 func TestYAMLEq_HashAndArrayNotEquivalent(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, `["foo", {"hello": "world", "nested": "hash"}]`, `{"foo": "bar", {"nested": "hash", "hello": "world"}}`))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, `["foo", {"hello": "world", "nested": "hash"}]`, `{"foo": "bar", {"nested": "hash", "hello": "world"}}`))
 }
 
 func TestYAMLEq_HashesNotEquivalent(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, `{"foo": "bar"}`, `{"foo": "bar", "hello": "world"}`))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, `{"foo": "bar"}`, `{"foo": "bar", "hello": "world"}`))
 }
 
 func TestYAMLEq_ActualIsSimpleString(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, `{"foo": "bar"}`, "Simple String"))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, `{"foo": "bar"}`, "Simple String"))
 }
 
 func TestYAMLEq_ExpectedIsSimpleString(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, "Simple String", `{"foo": "bar", "hello": "world"}`))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, "Simple String", `{"foo": "bar", "hello": "world"}`))
 }
 
 func TestYAMLEq_ExpectedAndActualSimpleString(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT, "Simple String", "Simple String"))
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock, "Simple String", "Simple String"))
 }
 
 func TestYAMLEq_ArraysOfDifferentOrder(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, `["foo", {"hello": "world", "nested": "hash"}]`, `[{ "hello": "world", "nested": "hash"}, "foo"]`))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, `["foo", {"hello": "world", "nested": "hash"}]`, `[{ "hello": "world", "nested": "hash"}, "foo"]`))
 }
 
 func TestYAMLEq_OnlyFirstDocument(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.True(t, target.YAMLEq(mockT,
+	mock := new(testing.T)
+	target.True(t, target.YAMLEq(mock,
 		`---
 doc1: same
 ---
@@ -100,6 +100,23 @@ doc2: notsame
 func TestYAMLEq_InvalidIdenticalYAML(t *testing.T) {
 	t.Parallel()
 
-	mockT := new(testing.T)
-	target.False(t, target.YAMLEq(mockT, `}`, `}`))
+	mock := new(testing.T)
+	target.False(t, target.YAMLEq(mock, `}`, `}`))
+}
+
+func TestYAMLUnmarshalAs(t *testing.T) {
+	t.Parallel()
+
+	mock := new(testing.T)
+	type dummy struct {
+		Hello string `yaml:"hello"`
+		Foo   string `yaml:"foo"`
+	}
+
+	value := dummy{Hello: "world", Foo: "bar"}
+	target.False(t, target.YAMLUnmarshalAsT(mock, value, `{"hello": "buzz", "foo": "lightyear"}`))
+	target.False(t, target.YAMLMarshalAsT(mock, `{"hello": "buzz", "foo": "lightyear"}`, value))
+
+	target.True(t, target.YAMLUnmarshalAsT(mock, value, `{"hello": "world", "foo": "bar"}`))
+	target.True(t, target.YAMLMarshalAsT(mock, `{"hello": "world", "foo": "bar"}`, value))
 }
