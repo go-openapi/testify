0c070fbadfdccd04db49fb3e2793fa3ab5521d5d
Frederic BIDON <fredbi@yahoo.com>
2026-02-10 23:04:59 +0100

    test: added tests to cover theme customization in enable/stubs
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- enable/stubs/colors/enable_colors_test.go
+++ enable/stubs/colors/enable_colors_test.go
@@ -4,6 +4,7 @@
 package colors
 
 import (
+	"fmt"
 	"testing"
 
 	target "github.com/go-openapi/testify/v2/assert"
@@ -11,18 +12,48 @@ import (
 )
 
 func TestEnableColors(t *testing.T) {
-	t.Parallel()
+	// Test execution is serialized on the sensitive package initialization section
+	//
+	// Proper test coverage of Enable() is assured by the testintegration package.
 
+	for _, opt := range []Option{
+		WithSanitizedTheme("light"),
+		WithDark(),
+		WithLight(),
+		WithTheme(ThemeDark),
+	} {
+		mock := new(mockT)
+		target.NotPanics(mock, func() {
+			testEnableWithLock(opt) // executed serially
+
+			_ = target.JSONEq(mock, `{"hello": "world", "foo": "bar"}`, `{"hello": "worldwide", "foo": "bar"}`)
+		})
+		// we may call several times with different options, but only the first setup is going to be used:
+		// colorization is set lazily with a sync.Once global by the consuming package.
+		t.Log(mock.errorString())
+	}
+}
+
+func testEnableWithLock(opt Option) {
 	colorstub.Enable(
 		func() []colorstub.Option {
 			return []colorstub.Option{
 				colorstub.WithEnable(true),
-				colorstub.WithSanitizedTheme("light"),
+				opt,
 			}
 		})
+}
 
-	mock := new(testing.T)
-	target.NotPanics(mock, func() {
-		_ = target.JSONEq(mock, `{"hello": "world", "foo": "bar"}`, `{"hello": "worldwide", "foo": "bar"}`)
-	})
+type mockT struct {
+	errorFmt string
+	args     []any
+}
+
+func (m *mockT) Errorf(format string, args ...any) {
+	m.errorFmt = format
+	m.args = args
+}
+
+func (m *mockT) errorString() string {
+	return fmt.Sprintf(m.errorFmt, m.args...)
 }
